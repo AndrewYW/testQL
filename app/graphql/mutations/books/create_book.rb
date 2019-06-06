9772888cb42a6f853a470eb503265414bb69c75a
AndrewYW <andrewwang39@gmail.com>
2019-06-06 14:56:12 -0400

    add RUD mutations

---
+++ app/graphql/mutations/books/create_book.rb
@@ -0,0 +1,14 @@
+module Mutations
+  class Books::CreateBook < ::Mutations::BaseMutation
+    argument :title, String, required: true
+    argument :author_id, Integer, required: true
+    argument :publication_date, Integer, required: true
+    argument :genre, Types::Enums::Genre, required: true
+
+    type Types::BookType
+
+    def resolve(author_id:, **attributes)
+      Author.find(author_id).books.create!(attributes)
+    end
+  end
+end
