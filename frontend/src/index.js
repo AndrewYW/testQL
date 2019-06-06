29a7741463ac2aa9846e9ff6a63d18ddabd8915f
AndrewYW <andrewwang39@gmail.com>
2019-06-06 16:43:22 -0400

    update index to make first graphql query

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+
+const client = new ApolloClient({
+  uri: "/graphql"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
+client
+  .query({
+    query: gql `
+      {
+        authors {
+          firstName
+          lastName
+          books {
+            title
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
