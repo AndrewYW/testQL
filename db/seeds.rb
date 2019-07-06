5699b04045905f3d1146c1279d80a17bfda15cda
AndrewYW <andrewwang39@gmail.com>
2019-07-05 20:33:30 -0700

    update seed for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
 Book.destroy_all
 Author.destroy_all
 
@@ -13,8 +14,10 @@ ActiveRecord::Base.connection.tables.each do |t|
   ActiveRecord::Base.connection.reset_pk_sequence!(t)
 end
 
-stephen = Author.create(first_name: 'Stephen', last_name: 'King', date_of_birth: Date.parse('1947-09-21'))
-lee     = Author.create(first_name: 'Lee', last_name: 'Child', date_of_birth: Date.parse('1954-10-29'))
+demouser = User.create(first_name: "demo", last_name: "user", email: "demo@email.com", password: "secret")
+
+stephen = Author.create(first_name: 'Stephen', last_name: 'King', date_of_birth: Date.parse('1947-09-21'), user_id: demouser.id)
+lee     = Author.create(first_name: 'Lee', last_name: 'Child', date_of_birth: Date.parse('1954-10-29'), user_id: demouser.id)
 
 Book.create(title: 'The Shining', author: stephen, publication_date: 1977, genre: 'Horror')
 Book.create(title: 'Carrie', author: stephen, publication_date: 1974, genre: 'Horror')
